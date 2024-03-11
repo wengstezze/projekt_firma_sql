a8bb2d5584f57463c2988f96bb44a37370f441d7
Tomasz Szmuc <tomekszmuc@interia.pl>
2024-03-11 21:02:40 +0100

    All points from instruction - done

--- firma.sql
+++ firma.sql
@@ -1,4 +1,4 @@
-#Aby zrealizować projekt, njapierw musimy utworzyć bazę danych o danej nazwie, jaką sobie ustalimy.
+#Aby zrealizować projekt, najpierw musimy utworzyć bazę danych o danej nazwie, jaką sobie ustalimy.
 #W moim przypadku jest to nazwa projekt_firma. W drugim poleceniu wybraliśmy utworzoną bazę, aby na niej pracować:
 
 CREATE DATABASE projekt_firma;
@@ -63,3 +63,48 @@ SELECT pracownicy.numer_wydz
 COUNT(*) ilosc_pracownikow
 FROM pracownicy
 GROUP BY pracownicy.numer_wydz;
+
+
+#(7)Utworzyć kwerendę wyświetlającą pracowników z tego samego wydziału co Ewa Nowacka (należy użyć zapytania z podzapytaniem):
+
+SELECT pracownicy.imie_i_nazw, pracownicy.numer_wydz
+FROM pracownicy 
+WHERE pracownicy.numer_wydz = (SELECT pracownicy.numer_wydz
+							   FROM pracownicy
+							   WHERE pracownicy.imie_i_nazw = "Ewa Nowacka");
+
+
+#uwaga: kwerendy (8)-(9) dotyczą danych z dwóch tabel
+#(8)Utworzyć kwerendę wyświetlającą wszystkich pracowników wraz z podaniem numeru wydziału i nazwy wydziału (należy
+#użyć słowa kluczowego INNER JOIN):
+
+SELECT pracownicy.imie_i_nazw, wydzialy.numer_wydz, wydzialy.nazwa_wydz
+FROM pracownicy
+INNER JOIN wydzialy ON wydzialy.numer_wydz = pracownicy.numer_wydz;
+
+
+#(9)Utworzyć kwerendę zliczającą pracowników dla każdej nazwy wydziału (należy użyć funkcji COUNT i komendy GROUP BY):
+
+SELECT wydzialy.nazwa_wydz, 
+	   COUNT(pracownicy.id_prac) ilosc_pracownikow
+FROM pracownicy
+INNER JOIN wydzialy ON wydzialy.numer_wydz = pracownicy.numer_wydz
+GROUP BY wydzialy.nazwa_wydz;
+
+
+#(10)Utworzyć relację jeden do wielu pomiędzy tabelami:
+
+ALTER TABLE pracownicy
+ADD CONSTRAINT pracownicy_ref1
+FOREIGN KEY (wydzialy)
+REFERENCES wydzialy (numer_wydz);
+
+
+#(11)Pokazać na przykładzie, że relacja działa prawidłowo (np. poprzez próbę dopisania pracownika z niewłaściwym numerem wydziału):
+
+INSERT INTO pracownicy (id_prac, imie_i_nazw, d_ur, numer_wydz) 
+VALUES (8525, 'Adam Małysz', STR_TO_DATE('1960-09-19', '%Y-%m-%d'), 12);
+
+
+
+
